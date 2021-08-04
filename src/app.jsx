9f3e5364da6ec6f20669e86fa34048dd4e70de33
Jiyeon <yeeoni_@naver.com>
2021-08-04 18:25:35 +0900

    add login and logout

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,23 @@
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './app.css';
+import Login from './components/login/login';
+import Maker from './components/maker/maker';
 
-function App() {
-  return <h1>hello</h1>;
+function App({ authService }) {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+					<Route path="/maker">
+						<Maker authService={authService}/>
+					</Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
