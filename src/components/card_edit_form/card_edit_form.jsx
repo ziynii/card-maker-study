00d36a39fbcae16d36728d7d57b3beb96c7ecd6c
Jiyeon <yeeoni_@naver.com>
2021-08-05 23:33:11 +0900

    handle state change on editor

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,34 +1,86 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Button from '../button/button';
 import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
-const CardEditForm = ({ card }) => {
+const CardEditForm = ({ card, updateCard, deleteCard }) => {
+  const formRef = useRef();
+  const nameRef = useRef();
+  const themeRef = useRef();
+  const companyRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
   const { name, company, title, email, theme, fileName, fileURL, message } =
     card;
 
-  const onSubmit = () => {};
+  const onChange = (event) => {
+    if ((event.currnetTarget = null)) {
+      return;
+    }
+    event.preventDefault();
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  const onSubmit = (event) => {
+    deleteCard(card);
+  };
 
   return (
-    <form className={styles.form}>
-      <input className={styles.input} type="text" name="name" value={name} />
+    <form className={styles.form} ref={formRef}>
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        value={name}
+        onChange={onChange}
+        ref={nameRef}
+      />
       <input
         className={styles.input}
         type="text"
         name="company"
         value={company}
+        onChange={onChange}
+        ref={companyRef}
       />
-      <select className={styles.select} name="theme" value={theme}>
+      <select
+        className={styles.select}
+        name="theme"
+        value={theme}
+        onChange={onChange}
+        ref={themeRef}
+      >
         <option value="light">light</option>
         <option value="dark">dark</option>
         <option value="colorful">colorful</option>
       </select>
-      <input className={styles.input} type="text" name="title" value={title} />
-      <input className={styles.input} type="text" name="email" value={email} />
+      <input
+        className={styles.input}
+        type="text"
+        name="title"
+        value={title}
+        onChange={onChange}
+        ref={titleRef}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        value={email}
+        onChange={onChange}
+        ref={emailRef}
+      />
       <textarea
         className={styles.textarea}
         name="message"
         value={message}
+        onChange={onChange}
+        ref={messageRef}
       ></textarea>
       <div className={styles.fileInput}>
         <ImageFileInput />
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,41 +7,42 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
-	const [cards, setCards] = useState([
-		{
-			id: '1',
-			name: 'Ellie1',
-			company: 'Samsung',
-			theme: 'dark',
-			email: 'ellie@gmail.com',
-			title: 'Software Engineer',
-			message: 'go for it',
-			fileName: 'ellie',
-			fileURL: null,
-		},
-		{
-			id: '2',
-			name: 'Ellie2',
-			company: 'Samsung',
-			theme: 'light',
-			email: 'ellie@gmail.com',
-			title: 'Software Engineer',
-			message: 'go for it',
-			fileName: 'ellie',
-			fileURL: 'ellie.png'
-		},
-		{
-			id: '3',
-			name: 'Ellie3',
-			company: 'Samsung',
-			theme: 'colorful',
-			email: 'ellie@gmail.com',
-			title: 'Software Engineer',
-			message: 'go for it',
-			fileName: 'ellie',
-			fileURL: null,
-		},
-	]);
+  const [cards, setCards] = useState({
+    1: {
+      id: '1',
+      name: 'Ellie1',
+      company: 'Samsung',
+      theme: 'dark',
+      email: 'ellie@gmail.com',
+      title: 'Software Engineer',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    2: {
+      id: '2',
+      name: 'Ellie2',
+      company: 'Samsung',
+      theme: 'light',
+      email: 'ellie@gmail.com',
+      title: 'Software Engineer',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: 'ellie.png',
+    },
+    3: {
+      id: '3',
+      name: 'Ellie3',
+      company: 'Samsung',
+      theme: 'colorful',
+      email: 'ellie@gmail.com',
+      title: 'Software Engineer',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+  });
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -55,17 +56,33 @@ const Maker = ({ authService }) => {
     });
   });
 
-	const addCard = card => {
-		const update = [...cards, card];
-		setCards(update);
-	}
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+			return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+		setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+			return updated;
+    });
+  };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
-        <Preview cards={cards}/>
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
