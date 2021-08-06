e5bc402c873f11225085b260dd6233baaea99afc
Jiyeon <yeeoni_@naver.com>
2021-08-06 23:28:00 +0900

    add imageFileInput

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import AuthService from './service/auth_service';
+import ImageUploader from './service/image_uploader';
+import ImageFileInput from './components/image_file_input/image_file_input';
 
 const authService = new AuthService();
+const imageUploader = new ImageUploader();
+const FileInput = (props) => (
+  <ImageFileInput {...props} imageUploader={imageUploader} />
+);
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService}/>
+    <App authService={authService} />
   </React.StrictMode>,
   document.getElementById('root')
 );
